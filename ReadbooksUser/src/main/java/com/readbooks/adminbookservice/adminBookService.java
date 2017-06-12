846a2120be52132cedf1dd104c778bc770a7d225
SeungCheol LEE <treatyouB@gmail.com>
2017-06-12 09:32:56 +0900

    admin 제거

--- ReadbooksUser/src/main/java/com/readbooks/adminbookservice/adminBookService.java
+++
@@ -1,8 +0,0 @@
-package com.readbooks.adminbookservice;
-
-import com.readbooks.adminbookvo.AdminBookVO;
-
-public interface AdminbookService {
-
-	public int adminbookInsert(AdminBookVO AdminBookVO);
-}
